cd1f68dcadf1eb90b5b48ce76b1457420e94f941
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-16 17:47:12 +0700

    create cart and exporting it

---
+++ src/assets/cart/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Cart = () => {
+    return (
+        <svg
+            width='28'
+            height='30'
+            fill='none'
+            viewBox='0 0 28 30'
+            xmlns='http://www.w3.org/2000/svg'
+        >
+            <path
+                d='M5.5 0.833252L1.25 6.49992V26.3333C1.25 27.0847 1.54851 27.8054 2.07986 28.3367C2.61122 28.8681 3.33189 29.1666 4.08333 29.1666H23.9167C24.6681 29.1666 25.3888 28.8681 25.9201 28.3367C26.4515 27.8054 26.75 27.0847 26.75 26.3333V6.49992L22.5 0.833252H5.5Z'
+                stroke='#34251F'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+            <path
+                d='M1.25 6.5H26.75'
+                stroke='#34251F'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+            <path
+                d='M19.6666 12.1667C19.6666 13.6696 19.0696 15.111 18.0069 16.1737C16.9442 17.2364 15.5029 17.8334 14 17.8334C12.4971 17.8334 11.0557 17.2364 9.99304 16.1737C8.93033 15.111 8.33331 13.6696 8.33331 12.1667'
+                stroke='#34251F'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+        </svg>
+    )
+}
+
+export default Cart
