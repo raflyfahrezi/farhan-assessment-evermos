d85de6cacde6d1d58c20321f2d90ab591df29e45
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-16 14:42:31 +0700

    import font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,17 @@ import type { NextPage } from 'next'
 import { HomeModule } from '@/modules'
 import { PageWrapper } from '@/layout'
 
+import '@fontsource/lato/400.css'
+import '@fontsource/lato/600.css'
+import '@fontsource/lato/800.css'
+
+import '@fontsource/frank-ruhl-libre/400.css'
+import '@fontsource/frank-ruhl-libre/600.css'
+import '@fontsource/frank-ruhl-libre/800.css'
+
 const index: NextPage = () => {
     return (
-        <PageWrapper title='Home'>
+        <PageWrapper title='fahsyon'>
             <HomeModule />
         </PageWrapper>
     )
