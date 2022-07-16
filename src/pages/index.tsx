bfe3feb5171513b49a034fe111d15f3bec125118
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-16 15:07:08 +0700

    fix index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,6 @@ import type { NextPage } from 'next'
 import { HomeModule } from '@/modules'
 import { PageWrapper } from '@/layout'
 
-import '@fontsource/lato/400.css'
-import '@fontsource/lato/600.css'
-import '@fontsource/lato/800.css'
-
-import '@fontsource/frank-ruhl-libre/400.css'
-import '@fontsource/frank-ruhl-libre/600.css'
-import '@fontsource/frank-ruhl-libre/800.css'
-
 const index: NextPage = () => {
     return (
         <PageWrapper title='fahsyon'>
