136412a5a1fcab6f7ec17ef831659d325c524193
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-16 14:33:04 +0700

    add global styles

---
+++ src/styles/globals/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Global, css } from '@emotion/react'
+
+import { font } from '../theme'
+
+const globalCSS = css`
+    * {
+        margin: 0;
+        padding: 0;
+
+        box-sizing: border-box;
+
+        font-family: ${font.family};
+    }
+`
+
+const GlobalCSS = () => {
+    return <Global styles={globalCSS} />
+}
+
+export default GlobalCSS
