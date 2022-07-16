d248cac727612ebb3773045cbc6d4032f5b09210
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-16 17:46:48 +0700

    fix navigation

---
+++ src/layout/navigation/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { CartIcon } from '@/assets'
+import { Wrapper } from '@/components'
+import { Paragraph } from '@/typography'
+
+import {
+    sNavigation,
+    sNavigationLink,
+    sNavigationCart,
+    sNavigationWrapper,
+    sNavigationCartNumber,
+} from './styles'
+
+const Navigation = () => {
+    return (
+        <div className={sNavigation}>
+            <Wrapper>
+                <div className={sNavigationWrapper}>
+                    <div>
+                        <Paragraph>fahsyon</Paragraph>
+                    </div>
+                    <div className={sNavigationLink}>
+                        <Paragraph weight='bold'>Home</Paragraph>
+                        <Paragraph weight='bold'>Products</Paragraph>
+                    </div>
+                    <div>
+                        <div className={sNavigationCart}>
+                            <p className={sNavigationCartNumber}>0</p>
+                            <CartIcon />
+                        </div>
+                    </div>
+                </div>
+            </Wrapper>
+        </div>
+    )
+}
+
+export default Navigation
