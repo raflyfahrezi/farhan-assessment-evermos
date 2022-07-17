c5a74bebcd868c12f5aa28a190df1c9d5272563a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-17 15:08:25 +0700

    fix index pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,60 @@
 import React from 'react'
-import type { NextPage } from 'next'
+import { GetStaticPropsResult, InferGetStaticPropsType } from 'next'
 
+import { axiosGet } from '@/utils'
+import { Product } from '@/models'
 import { HomeModule } from '@/modules'
 import { PageWrapper } from '@/layout'
 
-const index: NextPage = () => {
+export interface IndexProps {
+    jeweleryList: Product[]
+    mensClothingList: Product[]
+    womensClothingList: Product[]
+}
+
+const index = ({
+    jeweleryList,
+    mensClothingList,
+    womensClothingList,
+}: InferGetStaticPropsType<IndexProps>) => {
     return (
         <PageWrapper title='fahsyon'>
-            <HomeModule />
+            <HomeModule
+                jeweleryList={jeweleryList}
+                mensClothingList={mensClothingList}
+                womensClothingList={womensClothingList}
+            />
         </PageWrapper>
     )
 }
 
+const getStaticProps = async (): Promise<GetStaticPropsResult<IndexProps>> => {
+    const mensClothingList = await axiosGet(
+        "/products/category/men's%20clothing",
+        {}
+    )
+
+    const womensClothingList = await axiosGet(
+        "/products/category/women's%20clothing",
+        {}
+    )
+
+    const jeweleryClothingList = await axiosGet(
+        '/products/category/jewelery',
+        {}
+    )
+
+    return {
+        props: {
+            jeweleryList: jeweleryClothingList.data,
+            mensClothingList: mensClothingList.data,
+            womensClothingList: womensClothingList.data,
+        },
+
+        // Incremental Static Rendering (ISR)
+        revalidate: 60,
+    }
+}
+
 export default index
+export { getStaticProps }
