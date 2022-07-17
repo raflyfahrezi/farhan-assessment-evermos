85de5c73fb35dc992b46cd751e48f40e9e0462fa
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-17 19:03:30 +0700

    add new assets

---
+++ src/assets/star/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Star = () => {
+    return (
+        <svg
+            width='13'
+            height='12'
+            fill='none'
+            viewBox='0 0 13 12'
+            xmlns='http://www.w3.org/2000/svg'
+        >
+            <path
+                d='M6.5 0L8.5085 3.94953L13 4.58675L9.75 7.65931L10.517 12L6.5 9.94953L2.483 12L3.25 7.65931L0 4.58675L4.4915 3.94953L6.5 0Z'
+                fill='#FFB168'
+            />
+        </svg>
+    )
+}
+
+export default Star
