412716af3f91181f5dfbac2af86f6018ebd8b316
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-17 22:19:27 +0700

    create new pages

---
+++ src/pages/category/[slug]/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { ParsedUrlQuery } from 'querystring'
+import { GetStaticPaths, GetStaticProps } from 'next'
+
+import { Product } from '@/models'
+import { axiosGet } from '@/utils'
+import { PageWrapper } from '@/layout'
+import { CategoryModule } from '@/modules'
+
+interface ContextProps extends ParsedUrlQuery {
+    slug: string
+}
+
+interface CategoryProps {
+    slug: string
+    productList: Product[]
+}
+
+const category = ({ slug, productList }: CategoryProps) => {
+    return (
+        <PageWrapper
+            title={`${slug.toUpperCase()} Products - fahsyion`}
+            withPaddingTop
+        >
+            <CategoryModule title={slug} productList={productList} />
+        </PageWrapper>
+    )
+}
+
+const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await axiosGet('/products/categories', {})
+
+    const paths = data.map((slug: any) => {
+        return {
+            params: { slug },
+        }
+    })
+
+    return {
+        paths,
+        fallback: true,
+    }
+}
+
+const getStaticProps: GetStaticProps = async (context) => {
+    const { slug } = context.params as ContextProps
+
+    const productList = await axiosGet(`/products/category/${slug}`, {})
+
+    return {
+        props: {
+            slug,
+            productList: productList.data,
+        },
+        revalidate: 60,
+    }
+}
+
+export default category
+export { getStaticPaths, getStaticProps }
