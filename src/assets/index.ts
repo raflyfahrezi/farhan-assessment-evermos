3b8506382d98c25c042dcfbb7489f25d2b98a1e9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-18 14:14:27 +0700

    fix export asset

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,7 @@
 import StarIcon from './star'
 import CartIcon from './cart'
+import MedalIcon from './medal'
+import ShieldIcon from './shield'
+import PackageIcon from './package'
 
-export { StarIcon, CartIcon }
+export { StarIcon, CartIcon, MedalIcon, ShieldIcon, PackageIcon }
