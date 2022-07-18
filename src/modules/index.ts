20a6466a81b2354b1beeb12223a27bb07cc5ca52
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-18 16:56:00 +0700

    fix module export

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,5 @@
 import HomeModule from './home'
 import ProductModule from './product'
 import CategoryModule from './category'
-import ProductsModule from './products'
 
-export { HomeModule, ProductModule, CategoryModule, ProductsModule }
+export { HomeModule, ProductModule, CategoryModule }
