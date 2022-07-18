e953abdd61ece5d04542e3bea467d1e31f79e105
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-18 14:13:36 +0700

    adding new asset

---
+++ src/assets/package/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Package = () => {
+    return (
+        <svg
+            viewBox='0 0 24 24'
+            height='100%'
+            width='100%'
+            focusable='false'
+            role='img'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        >
+            <title>PackageIcon icon</title>
+            <line x1='16.5' x2='7.5' y1='9.4' y2='4.21' />
+            <path d='M21 16V8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z' />
+            <polyline points='3.27 6.96 12 12.01 20.73 6.96' />
+            <line x1='12' x2='12' y1='22.08' y2='12' />
+        </svg>
+    )
+}
+
+export default Package
