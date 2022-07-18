3b5f6c658b335902a29ac5026bec570b2b79527b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-18 22:40:44 +0700

    fix assets menu

---
+++ src/assets/menu/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            width='24'
+            height='24'
+            stroke='currentColor'
+            strokeWidth={2}
+        >
+            <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M4 6h16M4 12h16M4 18h7'
+            />
+        </svg>
+    )
+}
+
+export default Menu
